b6def108c108f1cd34907b52662db22872089e20
Uri Shaked <uri@salsa4fun.co.il>
2012-04-06 04:08:56 +0300

    implemented equals, hashCode, toString()

--- src/main/java/org/urish/openal/Tuple3F.java
+++ src/main/java/org/urish/openal/Tuple3F.java
@@ -16,4 +16,29 @@ public class Tuple3F {
 		this.v2 = v2;
 		this.v3 = v3;
 	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == null) {
+			return false;
+		}
+		if (!(other instanceof Tuple3F)) {
+			return false;
+		}
+		return (((Tuple3F) other).v1 == v1) && (((Tuple3F) other).v2 == v2) && (((Tuple3F) other).v3 == v3);
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 1;
+		hash = hash * 17 + Float.floatToIntBits(v1);
+		hash = hash * 31 + Float.floatToIntBits(v2);
+		hash = hash * 13 + Float.floatToIntBits(v3);
+		return hash;
+	}
+
+	@Override
+	public String toString() {
+		return "Tuple3F[" + v1 + "," + v2 + "," + v3 + "]";
+	}
 }
